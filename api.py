7e7ffbfaf1a84ac48e8c317fddc7a34f801e1c23
Antonio Hannesch Junior <hanneschjr@gmail.com>
2026-09-17 16:44:01 -0300

    Implementando o endpoint de leitura (GET)

---
+++ api.py
@@ -0,0 +1,33 @@
+from uuid import UUID, uuid4
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+# 1. Inicialização da API
+app = FastAPI(title="API de Livros", description="API para gerenciar livros", version="1.0.0")
+
+
+# 2. Base de dados em memória (dicionário)
+livros_db = {
+    1: {"id": uuid4(), "autor": "George Orwell", "titulo": "1984", "editora": "Companhia das Letras","ano": 1949},
+    2: {"id": uuid4(), "autor": "J.R.R. Tolkien", "titulo": "O Hobbit", "editora": "HarperCollins","ano": 1937},
+}
+
+
+# 3. Definição do modelo de dados
+class Livro(BaseModel):
+    id: UUID
+    autor: str
+    titulo: str
+    editora: str
+    ano: int
+
+# 4. Implementando o endpoint de leitura (GET)
+@app.get("/livros", response_model=List[Livro])
+async def listar_livros() -> List[Livro]:
+    return [Livro(**livro) for livro in livros_db.values()]      
+
+# if __name__ == "__main__":
+#     print(livros_db.values())
+#     print('#######################')
+#     print([Livro(**livro) for livro in livros_db.values()])       
